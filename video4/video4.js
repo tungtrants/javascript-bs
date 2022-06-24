e9ea741c3f56bc38bfa9faee9da3718f558b659b
tungtrants <tungtrants@gmail.com>
2022-06-24 15:23:51 +0700

    update video4

--- video4/video4.js
+++ video4/video4.js
@@ -1,5 +1,4 @@
-let a = 'I am string'
+let a = `I'm string`
+let text = `We are the so-called "Vikings" from the north.`;
+console.log(text)
 console.log(a)
-console.log(4+5+'a')
-
-console.log('a' + 5+ 4)

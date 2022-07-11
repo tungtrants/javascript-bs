be243eedb8a8a13091812d5159fbeb09b13d462d
tungtrants <tungtrants@gmail.com>
2022-07-11 17:01:12 +0700

    update video 7

---
+++ video6/video6.js
@@ -0,0 +1,10 @@
+console.log(`Hello world from HTML`.toUpperCase())
+let a = 5; //int, float, double ... JS không phân biệt các kiểu dữ liệu, không cần khai báo kiểu dữ liệu
+let b = 6;
+let c = 6.9;
+let d = '5'
+let e = +d//Number(d);
+console.log('e value: ', e, 'type: ', typeof e)
+// console.log(a + b)
+// console.log(a + c)
+console.log(a === d, 'a: ', typeof a, 'd: ',  typeof d)

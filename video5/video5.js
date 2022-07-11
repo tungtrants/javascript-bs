ad55774874201d5f26fa1d794a006e99fbc1a675
tungtrants <tungtrants@gmail.com>
2022-07-11 16:25:30 +0700

    update video 5 6

---
+++ video5/video5.js
@@ -0,0 +1,7 @@
+console.log("hello world from HTML")
+let text = "AApple, Banana, Kiwi";
+//document.getElementById("demo").innerHTML = text.length;
+console.log(text.length)
+console.log(text.substr(7,13))
+let text2 = text.toUpperCase();
+console.log(text2)

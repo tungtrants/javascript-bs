e0c44710ab803cd9b196acd36386a980a0a8e321
tungtrants <tungtrants@gmail.com>
2022-07-11 17:09:05 +0700

    update video 7

---
+++ video7/video7.js
@@ -0,0 +1,5 @@
+console.log('Hello world from HTML')
+let a = 5;
+let b = 10;
+console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.'); // \n = xuống dòng
+console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.'); // \n = xuống dòng

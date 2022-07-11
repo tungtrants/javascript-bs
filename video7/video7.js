a01c98aaf27678e026288e93b4017863da7d70eb
tungtrants <tungtrants@gmail.com>
2022-07-11 17:31:04 +0700

    update video 7

--- video7/video7.js
+++ video7/video7.js
@@ -1,5 +1,35 @@
 console.log('Hello world from HTML')
 let a = 5;
 let b = 10;
+let c = 11;
 console.log('Fifteen is ' + (a + b) + ' and\nnot ' + (2 * a + b) + '.'); // \n = xuống dòng
 console.log('Fifteen is ' + a + b + ' and\nnot ' + (2 * a + b) + '.'); // \n = xuống dòng
+
+console.log(`Check string : a = ${a} b = ${b} a + b = ${a + b}`) //template string
+console.log(`/hoidanit/${a}`)
+
+
+// var jsonData = '[{"rank":"9","content":"Alon","UID":"5"},{"rank":"6","content":"Tala","UID":"6"}]';
+
+// $.ajax({
+//     url: '/echo/json/',
+//     type: 'POST',
+//     data: {
+//         json: jsonData
+//     },
+//     success: function (response) {
+//         var trHTML = '';
+//         $.each(response, function (i, item) {
+    let trHTML = '';
+            trHTML += `
+            <tr>
+                <td>${a}</td>
+                <td>${b}</td>
+                <td>${c}</td>
+            </tr>`
+            // trHTML += '<tr><td>' + item.rank + '</td><td>' + item.content + '</td><td>' + item.UID + '</td></tr>';
+        // });
+        console.log(trHTML)
+//         $('#records_table').append(trHTML);
+//     }
+// });

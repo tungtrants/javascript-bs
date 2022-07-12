3719e7df0935cb23f3624c4f336cf69455ca742f
tungtrants <tungtrants@gmail.com>
2022-07-12 13:57:30 +0700

    update video 9

---
+++ video9/video9.js
@@ -0,0 +1,21 @@
+console.log("Hello World from HTML");
+let a = {
+    name: 'harry',
+    age: 25
+}; //key: value
+
+console.log('check date object: ', a);
+console.log('My name\'s ', a.name);
+
+//array = [item1, item2, item3], bản chất của array là object
+let b = ['MU', 'Chelsea', 'Liverpool'];
+console.log('type of b: ', typeof b,b);
+console.log('the first item: ', b[0],' second item: ',b[1]);
+
+let family = [
+    {name: 'harry', age: 14},
+    {name: 'ron', age: 15}
+
+]
+
+console.log('my family: ', family[0], family[1]);

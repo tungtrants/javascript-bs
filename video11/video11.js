e4a8d7651cf5496875f66da97eb0ba34649d4f13
tungtrants <tungtrants@gmail.com>
2022-07-13 16:29:29 +0700

    update video11

--- video11/video11.js
+++ video11/video11.js
@@ -1,5 +1,13 @@
 console.log('hello world from HTML')
 
-for (let i=0;i<10;i++){
-    console.log('check variable i= ',i)
+let arr = ['Mu', 'Mancity', 'Liverpool', 'chelsea']
+for (var i = 0; i < arr.length; ++i){
+    console.log('check variable i= ',arr[i]);
 }
+
+console.log('check variable i= ',i);
+
+//không dùng var thường xuyên bởi vì phạm vi sử dụng rộng
+
+//i++ === i = i + 1;
+//++i;

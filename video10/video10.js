aaaf6170d25490da72fb286de37397ca0b5e41fb
tungtrants <tungtrants@gmail.com>
2022-07-13 14:55:36 +0700

    update video 10

---
+++ video10/video10.js
@@ -0,0 +1,17 @@
+console.log('hello world from HTML')
+let a = 5;
+let b = '5';
+
+console.log('Compare a, b: ', 'a = ',a,',','b = ',b,', result: ',a === b);
+console.log('Compare a, b: ', 'a = ',a,',','b = ',b,', result: ',a == b);
+
+// === so sánh type kiểu dữ liệu, sau đó mới so sánh value 
+// == chỉ so sánh value
+
+//Empty, null, undefined
+// không định nghĩa thì là not defined hoặc undefined ví dụ: let c;
+// Empty ví dụ let c = {} => Empty
+// Empty ví dụ let c = null hoặc ''
+
+let c = null;
+console.log('check value: ',c);
